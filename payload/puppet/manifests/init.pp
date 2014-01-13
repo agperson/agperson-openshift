f188d1ae60dc98b40876057f18de1eb389ad53ea
Daniel Silverman <me@agperson.com>
2014-01-13 12:08:01 -0500

    Bootstrap suitable for F19 and initial all-in-one Puppetfile and manifest.

--- payload/puppet/manifests/init.pp
+++ payload/puppet/manifests/init.pp
@@ -1,5 +1,13 @@
 node default {
-  stage { 'pre': before => Stage['main'] }
-  class { 'epel': stage => 'pre' }
-  class { 'common': }
+  class { 'openshift_origin' :
+    domain                     => 'dev.oo.huit.harvard.edu',
+    roles                      => ['broker','node','activemq','datastore','named'],
+    node_unmanaged_users       => ['root'],
+    development_mode           => true,
+    install_login_shell        => true,
+    install_method             => 'yum',
+    register_host_with_named   => true,
+    broker_auth_plugin         => 'htpasswd',
+    broker_dns_plugin          => 'nsupdate',
+  }
 }
