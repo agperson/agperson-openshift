03fe6edf09d328085307603f5f6d8fe50e029521
Daniel Silverman <me@agperson.com>
2014-03-04 07:39:50 -0500

    Fix for vim-minimal install error

--- payload/puppet/site/base/manifests/init.pp
+++ payload/puppet/site/base/manifests/init.pp
@@ -5,4 +5,11 @@ class base {
   # OpenShift Origin Dependencies
   package { 'NetworkManager': }
   package { 'augeas': }
+
+  # OpenShift want to install vim-enhanced which conflicts with vim-minimal, a
+  # sudo dependency. Remove it while leaving sudo so that vim-enhanced can be
+  # cleanly installed without error.
+  exec { '/bin/rpm -e --nodeps vim-minimal':
+    onlyif => '/bin/rpm -q vim-minimal'
+  }
 }
